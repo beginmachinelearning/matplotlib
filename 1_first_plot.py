fd3019b477f012458e52eb58278cb59a8df29d13
Maxim Dsouza <maximdsouza87@gmail.com>
2018-03-31 09:10:29 +0530

    matplotlib

--- 1_first_plot.py
+++ 1_first_plot.py
@@ -6,13 +6,20 @@ x = np.array([2, 4, 5, 7, 10])
 
 y = np.array([1, 3, 5, 6, 8])
 # Plot the data
+fig = plt.figure(figsize=(6,6)) 
 #g for green, o for marker, - for line
-plt.plot(x, y, 'go-', label='Line Graph')
-
+plt.plot(x, y, 'go-')
+plt.title('My First Graph')
+plt.xlabel("X values")
+plt.ylabel("Y values")
 # Add a legend
-plt.legend()
+plt.legend(['Line Graph'])
 
 # Show the plot
 plt.show()
+
+
+
+
 
 
